1b06196c9863e26b9574c97562cc4d7c4f5a03d7
Richard Cool <rcool@mmto.org>
2016-01-26 09:22:33 -0700

    first commit

---
+++ headerparse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import pyfits
+import sys
+import re
+
+
+def main(argv):
+    
+    for ii in range(1, len(argv)):
+        print(argv)
+        
+
+if __name__ == "__main__":
+    main(sys.argv)
